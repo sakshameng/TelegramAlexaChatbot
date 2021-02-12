49e0c9e1d2c0b323ca2b66842b0c1ff9daa10cfd
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-12 17:23:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+from rivescript import RiveScript
+import os.path
 
+file = os.path.dirname(__file__)
+mind = os.path.file(file, 'brain')
+
+bot = RiveScript()
+bot.load_directory(mind)
+bot.sort_replies()
+
+while True:
+  msg = str(input('You> '))
+  
+  if msg == 'q':
+    break
+    
+  else:
+    print('Bot>'+bot.reply('localuser', msg))
