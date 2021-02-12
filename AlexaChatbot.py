d57bc9b7456ab32b24e48ebf6c0a6788e471bee9
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-12 19:29:49 +0530

    Create AlexaChatbot.py

---
+++ AlexaChatbot.py
@@ -0,0 +1,49 @@
+import sys
+import os
+from threading import Thread
+from telegram.ext import Updater, CommandHandler, Filters
+
+from handlers import all_handlers
+
+
+updater = Updater(os.environ.get("TOKEN"))
+
+for handler in all_handlers:
+    if len(handler) == 2:
+        updater.dispatcher.add_handler(
+            handler[0],
+            handler[1]
+        )
+    else:
+        updater.dispatcher.add_handler(
+            handler[0]
+        )
+
+
+def stop_and_restart():
+    updater.stop()
+    os.system("git pull")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+def restart(update, contex):
+    update.message.reply_text("Restarting...")
+    Thread(
+        target=stop_and_restart
+    ).start()
+
+
+updater.dispatcher.add_handler(
+    CommandHandler(
+        "r",
+        restart,
+        filters=Filters.user(
+            [
+                id,
+                id
+            ]
+        )
+    )
+)
+updater.start_polling()
+updater.idle()
