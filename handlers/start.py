3e9ad908e7031fd1d0e273c3402e08e971dfd1c3
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-13 09:57:28 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,7 +3,7 @@ from telegram.ext import CommandHandler
 
 def start(update, context):
     update.effective_message.reply_text(
-        "Hey there, I'm alive."
+        "Hey there, I'm alive.\nI am an AI ChatBot, Developed with love for interacting with users!"
     )
 
 
